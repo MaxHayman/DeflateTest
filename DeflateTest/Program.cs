2437e784a3a0fc1009d6f7ac27a6906d38dec99a
Max Hayman <maxhayman@maxhayman.co.uk>
2023-12-28 12:45:55 +0000

    fix: can build in net40

--- DeflateTest/Program.cs
+++ DeflateTest/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System;
+using System.IO;
 using System.IO.Compression;
 
 public class Program
@@ -12,25 +14,31 @@ public class Program
         s_messageBytes = File.ReadAllBytes("50MB.zip");
 
         Console.WriteLine($"The original string length is {s_messageBytes.Length} bytes.");
-        using var stream = new MemoryStream();
-        CompressBytesToStream(stream);
-        Console.WriteLine($"The compressed stream length is {stream.Length} bytes.");
-        int decompressedLength = DecompressStreamToBytes(stream);
-        Console.WriteLine($"The decompressed string length is {decompressedLength} bytes, same as the original length.");
-        /*
-         Output:
-            The original string length is 445 bytes.
-            The compressed stream length is 265 bytes.
-            The decompressed string length is 445 bytes, same as the original length.
-        */
+        using(var stream = new MemoryStream())
+        {
+
+            CompressBytesToStream(stream);
+            Console.WriteLine($"The compressed stream length is {stream.Length} bytes.");
+            int decompressedLength = DecompressStreamToBytes(stream);
+            Console.WriteLine($"The decompressed string length is {decompressedLength} bytes, same as the original length.");
+            /*
+             Output:
+                The original string length is 445 bytes.
+                The compressed stream length is 265 bytes.
+                The decompressed string length is 445 bytes, same as the original length.
+            */
+
+        }
     }
 
 
 
     private static void CompressBytesToStream(Stream stream)
     {
-        using var compressor = new DeflateStream(stream, CompressionMode.Compress, leaveOpen: true);
-        compressor.Write(s_messageBytes, 0, s_messageBytes.Length);
+        using (var compressor = new DeflateStream(stream, CompressionMode.Compress, leaveOpen: true))
+        {
+            compressor.Write(s_messageBytes, 0, s_messageBytes.Length);
+        }
     }
 
     private static int DecompressStreamToBytes(Stream stream)
@@ -38,8 +46,10 @@ public class Program
         stream.Position = 0;
         int bufferSize = 1000000000;
         byte[] decompressedBytes = new byte[bufferSize];
-        using var decompressor = new DeflateStream(stream, CompressionMode.Decompress);
-        int length = decompressor.Read(decompressedBytes, 0, bufferSize);
-        return length;
+        using (var decompressor = new DeflateStream(stream, CompressionMode.Decompress))
+        {
+            int length = decompressor.Read(decompressedBytes, 0, bufferSize);
+            return length;
+        }
     }
 }
